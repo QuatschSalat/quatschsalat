91178ede019429db1f6c077ebeaeb6ce4d1daea4
Artur Machura <artur@artur-machura.de>
2022-02-27 23:06:16 +0100

    Initial commit, basic release

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        itchio: '#fa5c5c',
+        itchioHover: '#f72347',
+        instagram: '#8a3ab9',
+        instagramHover: '#bc2a8d',
+        twitch: '#9146ff',
+        twitchHover: '#000000',
+        github: '#0F0C0D',
+        githubHover: '#707070'
+      }
+    },
+  },
+  plugins: [],
+}
